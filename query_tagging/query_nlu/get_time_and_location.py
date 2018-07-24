bd0ad919367d42d3f26a95cb417e7e177c43dc45
Soumya Batra <soumya.batra@gmail.com>
2018-07-23 19:25:27 -0700

    Corrected version

--- query_tagging/query_nlu/get_time_and_location.py
+++ query_tagging/query_nlu/get_time_and_location.py
@@ -1,13 +1,22 @@
-import requests
+import requests, sys
 
-url = "http://generic-resolver-prod-flight.trafficmanager.net/api/resolver/genericlu"
-query = "setup a meeting tomorrow 3 pm at bellevue"
-timexdomain= "timex"
-locationdomain = "location"
+def get_nlu_tags(sysargs):
+    url = "http://generic-resolver-prod-flight.trafficmanager.net/api/resolver/genericlu"
+    query = sysargs[0]
+    timexdomain= "timex"
+    locationdomain = "location"
+    nlu_tags = {}
 
-data = {'query': query, 'domains': [timexdomain, locationdomain]}
-print(requests.get(url, data).text)
+    data = {'query': query, 'domains': timexdomain}
+    nlu_tags['timex'] = requests.get(url, data).text
 
-#data['domains'] = locationdomain
-#print(requests.get(url, data).text)
+    data['domains'] = locationdomain
+    nlu_tags['location'] = requests.get(url, data).text
+
+    return nlu_tags
+
+
+# Main method.
+if __name__ == '__main__':
+    get_nlu_tags(sys.argv[1:])
 
