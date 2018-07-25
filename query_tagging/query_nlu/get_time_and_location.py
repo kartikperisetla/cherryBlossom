3ddaf07eb16e3b84afbef34dfe22ba8541e38214
Soumya Batra <soumya.batra@gmail.com>
2018-07-24 22:50:44 -0700

    Fixed a bug

--- query_tagging/query_nlu/get_time_and_location.py
+++ query_tagging/query_nlu/get_time_and_location.py
@@ -9,21 +9,31 @@ def get_nlu_tags(sysargs):
     nlu_tags = defaultdict(list)
 
     data = {'query': query, 'domains': timexdomain}
-    timex = json.loads(requests.get(url, data).text)['Domains'][0]['Item']['Slots']['Item']
+    timex = json.loads(requests.get(url, data).text)
+    
+    if timex is not None:
+        timex = timex.get('Domains',[{}])[0].get('Item',{}).get('Slots',{})
+        if timex is not None:
+            timex = timex.get('Item',[])
 
-    for tag in timex:
-        
-        resolved_values =  [ [ entity['Value'] for entity in propertygroup['Properties'] 
-                                                           if entity['Name'] == 'value'][0] for propertygroup 
-                                                           in tag['ResolutionInfo']['PropertyGroups'] ]
-        nlu_tags[tag['Name']].append({'OriginalText': tag['Text'], 'CanonicalValue': tag['ResolutionInfo']['CanonicalValue'],
-                                      'ResolvedValues': resolved_values})
+            for tag in timex:
+                
+                resolved_values =  [ [ entity['Value'] for entity in propertygroup['Properties'] 
+                                                                if entity['Name'] == 'value'][0] for propertygroup 
+                                                                in tag['ResolutionInfo']['PropertyGroups'] ]
+                nlu_tags[tag['Name']].append({'OriginalText': tag['Text'], 'CanonicalValue': tag['ResolutionInfo']['CanonicalValue'],
+                                            'ResolvedValues': resolved_values})
 
     data['domains'] = locationdomain
-    location = json.loads(requests.get(url, data).text)['Domains'][0]['Item']['Slots']['Item']
+    location = json.loads(requests.get(url, data).text)
+    
+    if location is not None:
+        location = location.get('Domains',[{}])[0].get('Item',{}).get('Slots',{})
+        if location is not None:
+            location = location.get('Item',[])
 
-    for tag in location:
-        nlu_tags[tag['Name']].append({'Place': tag['Text'], 'ResolutionInfo': tag['ResolutionInfo']})
+            for tag in location:
+                nlu_tags[tag['Name']].append({'Place': tag['Text'], 'ResolutionInfo': tag['ResolutionInfo']})
         
 
     return nlu_tags
