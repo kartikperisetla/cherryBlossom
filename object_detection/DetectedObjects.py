ec205683be9ab2243355e4bb46f747b0ce7ab4ad
Kartik Perisetla <kartik.peri@gmail.com>
2018-07-25 06:11:30 -0700

    removing old Detected object class

--- object_detection/DetectedObjects.py
+++
@@ -1,7 +0,0 @@
-class DetectedObjects:
-    def __init__(self, name, ymin,xmin,ymax,xmax, confidence):
-        self.name = name
-        self.boundary = [ ymin, xmin, ymax, xmax]
-        self.confidence = confidence
-
-    
